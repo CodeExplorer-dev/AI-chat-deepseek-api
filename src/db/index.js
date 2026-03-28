adb9f0a0d48906708f05f7e90a656cc40eb66f21
XilongWang <3382272471@qq.com>
2026-03-28 10:14:47 +0800

    修改数据库配置

--- src/db/index.js
+++ src/db/index.js
@@ -2,10 +2,13 @@ const mysql = require('mysql');
 require('dotenv').config()
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT || 3360,
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB_NAME || 'ai_chat',
+  waitForConnections: true,
+  connectionLimit: 10
 })
 
 module.exports = db
